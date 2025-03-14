719e6c42502c115ff29728462325726295607900
kishore kumar <kishornaveen2193@GMAIL.COM>
2025-03-15 01:00:12 +0530

    Update index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,30 +3,36 @@ import Socials from "../Socials";
 import Link from "next/link";
 import Button from "../Button";
 
-const Footer = ({}) => {
+const Footer = () => {
   return (
     <>
       <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
         <div>
-          <h1 className="text-2xl text-bold">Contact.</h1>
+          <h1 className="text-2xl font-bold">Contact.</h1>
           <div className="mt-10">
-            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl font-bold">
               LET&apos;S WORK
             </h1>
-            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl font-bold">
               TOGETHER
             </h1>
-            <Button type="primary">Schedule a call</Button>
+            <a
+              href="https://calendly.com/kishor47gaming/30min" 
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button type="primary">Schedule a Call</Button>
+            </a>
             <div className="mt-10">
               <Socials />
             </div>
           </div>
         </div>
       </div>
-      <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
+      <h1 className="text-sm font-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
-        <Link href="http://www.porfolio-kishor.com">
-          <a className="underline underline-offset-1">Kishore kumar</a>
+        <Link href="http://www.portfolio-kishor.com">
+          <a className="underline underline-offset-1">Kishore Kumar</a>
         </Link>
       </h1>
     </>
