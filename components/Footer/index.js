b55fde96180803f6816dd694b5dfd3b2c2457da9
kishore kumar <kishornaveen2193@GMAIL.COM>
2025-03-15 01:50:54 +0530

    Update index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { useRef } from "react";
 import Socials from "../Socials";
 import Link from "next/link";
 import Button from "../Button";
 
-const Footer = () => {
+const Footer = ({contactRef}) => {
+  
   return (
     <>
-      <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
+      <div className="mt-5 laptop:mt-40 p-2 laptop:p-0" ref={contactRef}>
         <div>
           <h1 className="text-2xl font-bold">Contact.</h1>
           <div className="mt-10">
